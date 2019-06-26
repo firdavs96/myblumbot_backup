b3861ac23f5a58d96572b8efaf483b0bd6ad27b9
Alisher Matkurbanov <alishermatkurbanov0408@gmail.com>
2019-06-26 18:24:16 +0500

    some refactoring

--- bot/shelver.py
+++ bot/shelver.py
@@ -4,10 +4,9 @@ import shelve
 import config
 
 
-class Shelver:
-	conn = None
-
-	def __init__(self, shelve_name=config.shelve_name):
+class Shelver(object):
+	def __init__(self, shelve_name=None):
+		shelve_name = shelve_name or config.shelve_name
 		self.conn = shelve.open(shelve_name, writeback=True)
 
 	def close(self):
@@ -19,15 +18,8 @@ class Shelver:
 	def __exit__(self, Type, Value, Trace):
 		self.close()
 
-	# TODO Test this func
 	def clear_shelve(self, shelve_name=None):
-		if shelve_name:
-			with self.__init__(shelve_name=shelve_name).conn as states:
-				for i in states:
-					print(i, ":", states[i])
-					del states[i]
-		else:
-			with self.__init__().conn as states:
-				for i in states:
-					print(i, ":", states[i])
-					del states[i]
+		with self.__init__(shelve_name).conn as states:
+			for i in states:
+				print(i, ":", states[i])
+				del states[i]
