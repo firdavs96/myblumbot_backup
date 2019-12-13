45bd7453981a4eafa62882645cea9328ed8eec84
Alisher Matkurbanov <alishermatkurbanov0408@gmail.com>
2019-12-13 15:19:51 +0500

    remove stream redirection

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@
 # shellcheck disable=SC2046
 export $(grep -v '^#' /media/amarus/DATA1/Programming/Bots/myblumbot_backup/.env | xargs)
 "$ROOT_DIR"/scripts/create_folders.sh
-"$ROOT_DIR"/venv/bin/python "$ROOT_DIR"/myblumbot/main.py 1>>"$ROOT_DIR"/logs/out.log 2>>"$ROOT_DIR"/logs/err.log
+"$ROOT_DIR"/venv/bin/python "$ROOT_DIR"/myblumbot/main.py
