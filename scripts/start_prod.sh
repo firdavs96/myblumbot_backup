20f7b6d4f6cee4ef151a11f0b8a214ed7f42e7b8
Alisher Matkurbanov <alishermatkurbanov0408@gmail.com>
2019-12-13 15:29:59 +0500

    fix

--- scripts/start_prod.sh
+++ scripts/start_prod.sh
@@ -2,4 +2,4 @@
 # shellcheck disable=SC2046
 export $(grep -v '^#' /home/ubuntu/bots/myblumbot_backup/.env | xargs)
 "$ROOT_DIR"/scripts/create_folders.sh
-"$ROOT_DIR"/venv/bin/python "$ROOT_DIR"/bot/main.py
+"$ROOT_DIR"/venv/bin/python "$ROOT_DIR"/myblumbot/main.py
