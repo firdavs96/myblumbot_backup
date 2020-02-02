f07aa2e436b99036f1e49a7e5e06561bfb2d0c9e
firdavs <firdavsmu96@gmail.com>
2020-02-02 18:25:36 +0500

    db_backups edit path

--- scripts/db_backups.sh
+++ scripts/db_backups.sh
@@ -1,5 +1,5 @@
 TIME=$(date +%b-%d-%y)
-export $(grep -v '^#' /root/myblum/.env | xargs)
+export $(grep -v '^#' /root/myblumbot/.env | xargs)
 FILENAME=backup-$TIME.tar.gz
 SRCDIR=$ROOT_DIR/BlumUz.db
 DESDIR=$ROOT_DIR/db_backups
